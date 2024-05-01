d4efbf55a86c027b1da1212f1ffa253a45eb46c7
Mohsen Amini <mohsenamini1081@gmail.com>
2024-05-01 21:48:34 +0330

    added final sections

--- app/(root)/(home)/recordings/page.tsx
+++ app/(root)/(home)/recordings/page.tsx
@@ -1,8 +1,13 @@
-const Recordings = () => {
+import CallList from "@/components/CallList";
+
+const PreviousPage = () => {
   return (
     <section className="flex size-full flex-col gap-10 text-white">
       <h1 className="text-3xl font-bold">Recordings</h1>
+
+      <CallList type="recordings" />
     </section>
   );
 };
-export default Recordings;
+
+export default PreviousPage;
